5d34212243324cf525214a276684031712f4261c
至臻肥仔 <lzydroper@163.com>
2026-08-22 13:56:37 +0800

    Update combatConfig.ts

--- src/data/combatConfig.ts
+++ src/data/combatConfig.ts
@@ -20,5 +20,5 @@ export const COMBAT_CONFIG: CombatConfig = {
   encounterStaminaCost: 5,
   battleDurationSeconds: 5,
   maxIdleSettlementSeconds: 8 * 3600,
-  baseEventIntervalMs: 500
+  baseEventIntervalMs: 1000
 };
